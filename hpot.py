dc91258ca5af5d2aee185c306bcdb5b3cddf2eda
Pranav <pranav.kulkarni1120@gmail.com>
2025-08-27 23:04:14 -0400

    finished all basic implementation to get password attempts

--- hpot.py
+++ hpot.py
@@ -1,7 +1,26 @@
 #!user/bin/env python3
 
 import socket
+import threading
 import paramiko
+
+class SSHServer(paramiko.ServerInterface):
+     def check_auth_password(self, username: str, password: str) -> int:
+          print(f"{username}:{password}")
+          return paramiko.AUTH_FAILED
+
+def handle_connection(client_socket):
+    #use to test TCP connection
+     #client_socket.send(b"hi\n")
+        #print(client_socket.recv(256).decode())
+
+        #use paramiko to run ssh server with the client socket
+        transport=paramiko.Transport(client_socket)
+        #key=paramiko.RSAKey.generate(2048)
+        key=paramiko.RSAKey.from_private_key_file('key')
+        transport.add_server_key(key)
+        ssh=SSHServer()
+        transport.start_server(server=ssh)
 
 #main
 def main():
@@ -10,19 +29,11 @@ def main():
     server_socket.bind(('',2222))
     server_socket.listen(223)
     
-    client_socket, client_address = server_socket.accept()
-    print(f"got connection from {client_address[0]}:{client_address[1]}")
-    
-        #use to test TCP connection
-    #client_socket.send(b"hi\n")
-    #print(client_socket.recv(256).decode())
-
-    #use paramiko to run ssh server with the client socket
-    transport=paramiko.Transport(client_socket)
-    key=paramiko.RSAKey.generate(2048)
-    transport.add_server_key(key)
-    ssh=paramiko.ServerInterface()
-    transport.start_server(server=ssh)
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"got connection from {client_address[0]}:{client_address[1]}")
+        t = threading.Thread(target=handle_connection, args=(client_socket,))
+        t.start()
 
 if __name__=="__main__":
     main()
